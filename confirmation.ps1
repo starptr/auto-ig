623e0181511c843e4882000a707dea4aa51e8662
Yuto Nishida <yuyu.nishida@gmail.com>
2020-08-22 22:32:30 -0700

    Add messagebox confirmation

---
+++ confirmation.ps1
@@ -0,0 +1,12 @@
+Add-Type -AssemblyName PresentationCore, PresentationFramework
+
+$Result = [System.Windows.MessageBox]::Show("Start Auto IG?", "Scheduled Auto IG", "OKCancel", "Question")
+
+if ($Result -eq "OK") {
+    # Invoke-Command -FilePath "$(Resolve-Path .\run.ps1)"
+    Set-Location -Path $PSScriptRoot
+    & .\run.ps1
+}
+else {
+    "Cancel"   
+}
